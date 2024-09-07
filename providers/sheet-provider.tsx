9462046fa458205703b9d2c9b905142ec9427339
keshav0100 <kbansal1_be22@thapar.edu>
2024-09-07 15:18:19 +0530

    Added Edit accounts

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,15 +1,15 @@
-'use client'
-import { useMountedState } from "react-use"
-import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet"
-import { useEffect, useState } from "react";
+"use client";
+import { useMountedState } from "react-use";
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
+export const SheetProvider = () => {
+  const isMounted = useMountedState();
 
-export const SheetProvider=()=>{
-    const isMounted=useMountedState();
-
-    if(!isMounted) return null;
-    return (
-        <>
-        <NewAccountSheet/>
-        </>
-    );
+  if (!isMounted) return null;
+  return (
+    <>
+      <NewAccountSheet />
+      <EditAccountSheet />
+    </>
+  );
 };
