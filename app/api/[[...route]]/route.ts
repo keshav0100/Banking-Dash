b5c12cec873c406542909dbec7fee4843e75e424
keshav0100 <kbansal1_be22@thapar.edu>
2024-09-16 13:14:24 +0530

    Add transaction management features and integrate date-fns library

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,19 +1,21 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import accounts  from './accounts'
-import categories  from './categories'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import accounts from "./accounts";
+import categories from "./categories";
+import transactions from "./transactions";
 // import { HTTPException } from 'hono/http-exception'
 
-export const runtime = 'edge'
-const app = new Hono().basePath("/api")
+export const runtime = "edge";
+const app = new Hono().basePath("/api");
 
 const routes = app
   .route("/accounts", accounts)
-  .route("/categories", categories);
+  .route("/categories", categories)
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
 export const POST = handle(app);
 
- export type AppType=typeof routes;
+export type AppType = typeof routes;
