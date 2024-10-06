02a20756e3924b50cf135e4e3996db8723be8f70
keshav0100 <kbansal1_be22@thapar.edu>
2024-10-06 10:54:00 +0530

    Refactor header and nav-button components

--- components/header.tsx
+++ components/header.tsx
@@ -7,8 +7,8 @@ import { WelcomeMsg } from "@/components/welcome-msg";
 
 export const Header = () => {
   return (
-    <div className="bg-gradient-to-b from-purple-700 to blue-500 px-4 py-8 lg:px-14 pb-36">
-      {/* <div className="bg-gradient-to-b from-[#12a7c2] to-[#0d7d8e] px-4 py-8 lg:px-14 pb-36"> */}
+    <div className="bg-gradient-to-b from-[#e99e79] to blue-500 px-4 py-8 lg:px-14 pb-36">
+      {/* // <div className="bg-gradient-to-b from-[#12a7c2] to-[#0d7d8e] px-4 py-8 lg:px-14 pb-36"> */}
 
       <div className="max-w-screen-2xl mx-auto ">
         <div className="w-full flex items-center justify-between mb-14">
