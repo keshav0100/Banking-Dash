af4d63a05edd9c49d3fd0df08f435407feec673c
keshav0100 <kbansal1_be22@thapar.edu>
2024-10-13 23:11:28 +0530

    Refactor dashboard page: Enable data grid and data charts components

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,12 +1,11 @@
 import { DataGrid } from "@/components/data-grid";
 import { DataCharts } from "@/components/data-charts";
 
-
 export default function DashBoardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
-      {/* <DataGrid /> */}
-      {/* <DataCharts /> */}
+      <DataGrid />
+      <DataCharts />
     </div>
   );
 }
